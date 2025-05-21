aa8d6e251af4255679c6fcb72a26fc2a229e12d3
Thaveesha Gamage <gamage.thaveesha@gmail.com>
2025-05-21 13:58:41 +0530

    commit code for rectngle

--- src/Chapter6/Rectangle.java
+++ src/Chapter6/Rectangle.java
@@ -1,4 +1,47 @@
 package Chapter6;
 
 public class Rectangle {
+
+    double length;
+    double width;
+
+    //Default Constructor
+    public Rectangle(){
+        length = 0;
+        width = 0;
+    }
+
+    //Constructor to add parameters
+    public Rectangle(double width, double length){
+        this.length = length;
+        setWidth(width);
+
+    }
+
+
+    //Get the stored length value
+    double getLength() {
+        return length;
+    }
+    //Set length from User
+    void setLength(double length){
+            this.length =length;
+    }
+    //Get the stored width value
+    double getWidth(){
+        return width;
+    }
+    //Set width from User
+    void setWidth(double width){
+        this.width = width;
+    }
+
+    //Calculate Perimeter
+    double calculatePerimeter(){
+            return (2 * length) + (2 * width);
+    }
+    //Calculate Area
+    double calculateArea(){
+        return width * length;
+    }
 }
