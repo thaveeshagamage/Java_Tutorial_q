e4972923819477eae8bc6454758158df5cf45b1a
Thaveesha Gamage <gamage.thaveesha@gmail.com>
2025-07-15 13:42:42 +0530

    commit chapter 6

---
+++ src/Chapter6/HomeAreaCalculatorRedo.java
@@ -0,0 +1,15 @@
+package Chapter6;
+
+public class HomeCalculatorRedo {
+
+    public static void main(String args[]){
+
+        Rectangle Kitchen = getRoom();
+        Rectangle Bathroom = getRoom();
+
+        
+
+
+    }
+
+}
